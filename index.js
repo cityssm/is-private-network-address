9bea6c3806f04ed8daf6d44ed1f371825c700c1c
dangowans <d.gowans@cityssm.on.ca>
2021-05-12 15:37:53 -0400

    adding another local address

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const localhost = "localhost";
 const localhost_v4_127 = "127.0.0.1";
 const localhost_v6 = "::1";
+const localhost_node = "::ffff:127.0.0.1";
 const range_v4_10 = /^10(\.\d{1,3}){3}$/;
 const range_v4_172 = /^172\.(1[6-9]|2[0-9]|3[0-1])(\.\d{1,3}){2}$/;
 const range_v4_192 = /^192\.168(\.\d{1,3}){2}$/;
@@ -10,6 +11,7 @@ export const isLocal = (networkAddress) => {
         case localhost:
         case localhost_v4_127:
         case localhost_v6:
+        case localhost_node:
             return true;
     }
     return false;
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@
 const localhost = "localhost";
 const localhost_v4_127 = "127.0.0.1";
 const localhost_v6 = "::1";
+const localhost_node = "::ffff:127.0.0.1";
 
 
 // Private Network Ranges
@@ -17,6 +18,7 @@ export const isLocal = (networkAddress: string) => {
     case localhost:
     case localhost_v4_127:
     case localhost_v6:
+    case localhost_node:
       return true;
   }
 
