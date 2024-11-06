7318e0ded5296fe2cbce9f3f96c53eb605c03c82
Dan Gowans <dangowans@users.noreply.github.com>
2024-11-06 09:50:14 -0500

    include jsdoc

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,17 @@
+/**
+ * Local Network Addresses
+ */
 export declare const localNetworkAddresses: Set<string>;
 export declare const privateNetworkRanges: RegExp[];
+/**
+ * Tests if a given network address is a local network address.
+ * @param networkAddress - A network address string.
+ * @returns `true` if the network address string represents a local network address.
+ */
 export declare function isLocal(networkAddress: string): boolean;
+/**
+ * Tests if a given network address is a private network address.
+ * @param networkAddress - A network address string.
+ * @returns `true` if the network address string represents a private network address.
+ */
 export declare function isPrivate(networkAddress: string): boolean;
--- index.js
+++ index.js
@@ -1,18 +1,34 @@
+/**
+ * Local Network Addresses
+ */
 export const localNetworkAddresses = new Set([
     'localhost',
     '127.0.0.1',
     '::1',
     '::ffff:127.0.0.1'
 ]);
+/*
+ * Private Network Ranges
+ */
 export const privateNetworkRanges = [
     /^(::ffff:)?10(\.\d{1,3}){3}$/,
     /^(::ffff:)?172\.(1[6-9]|2\d|3[01])(\.\d{1,3}){2}$/,
     /^(::ffff:)?192\.168(\.\d{1,3}){2}$/,
     /^f[cd][\da-f]{0,2}(:([\da-f]){0,4}){0,7}$/
 ];
+/**
+ * Tests if a given network address is a local network address.
+ * @param networkAddress - A network address string.
+ * @returns `true` if the network address string represents a local network address.
+ */
 export function isLocal(networkAddress) {
     return localNetworkAddresses.has(networkAddress.toLowerCase());
 }
+/**
+ * Tests if a given network address is a private network address.
+ * @param networkAddress - A network address string.
+ * @returns `true` if the network address string represents a private network address.
+ */
 export function isPrivate(networkAddress) {
     const networkAddressLowerCase = networkAddress.toLowerCase();
     for (const privateNetworkRange of privateNetworkRanges) {
